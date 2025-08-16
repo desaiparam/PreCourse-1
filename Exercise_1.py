95f14dcb3223f112fcea9ec66dcd071644509594
Param Desai <desaipd112@gmail.com>
2025-08-16 10:51:50 -0700

    Removed extra files

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,40 @@
+# Time Complexity :O(1) for all the methods 
+# Space Complexity : O(N) where N is the size of the stack
+# Did this code successfully run on Leetcode : I tried in VS code 
+# Any problem you faced while coding this :No
+
+
+# Your code here along with comments explaining your approach
+# In __init__ method I am intializing an array named stack to store the elements
+# In isEmpty I am check the if stack is not empty if not then return True 
+# In push I am adding the element to the top of the stack
+# In pop I am first checking if the stack is empty or not if not then removing the element from the top of the stack
+# In peek I am checking if the stack is empty or not if not then returning the element at the top of the stack
+# In size I am returning the size of the stack
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+    def __init__(self):
+         self.stack = []
          
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    def isEmpty(self):
+        if not self.stack:
+            return True
+        return False
+    def push(self, item):
+        self.stack.append(item)
+        return self.stack 
+    def pop(self):
+        if not self.stack:
+            return None
+        self.stack.pop()
+        return self.stack
+    def peek(self):
+        if not self.stack:
+            return None
+        return self.stack[-1]
+    def size(self):
+         return len(self.stack)
+    def show(self):
+         return self.stack
 
 s = myStack()
 s.push('1')
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,14 @@
+# Time Complexity :O(1) for all the methods 
+# Space Complexity : O(N) for the worst case where N is the size of the linkedlist 
+# Did this code successfully run on Leetcode : I tried in VS code 
+# Any problem you faced while coding this :No
 
+
+# Your code here along with comments explaining your approach
+# In __init__ method I am intializing the head as none for the linkedlist
+# In push I am adding the element to the top of the stack by using a new_head which will point to the current head
+# In pop I am first checking if the stack is empty or not if not then removing the element from the top of the stack
+# In peek I am checking if the stack is empty or not if not then removing the element at the top of the stack
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,10 +16,18 @@ class Node:
  
 class Stack:
     def __init__(self):
-        
+        self.head = None
+
     def push(self, data):
-        
+        new_head = Node(data)
+        new_head.next = self.head
+        self.head = new_head
     def pop(self):
+        if self.head is None:
+            return None
+        pop_head = self.head
+        self.head = self.head.next
+        return pop_head.data
         
 a_stack = Stack()
 while True:
